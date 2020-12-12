cad84aca31a93d4f9f0c28eceaea2d41f895edb4
namsoo5 <are34@naver.com>
2020-12-13 00:58:07 +0900

    UPDATE: 린트 적용

--- iOSCarePlus/ViewController.swift
+++ iOSCarePlus/ViewController.swift
@@ -8,12 +8,57 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet private weak var logoView: UIView!
+    @IBOutlet private weak var logoViewTopConstraint: NSLayoutConstraint!
+    @IBOutlet private weak var backgroundImageLeadingConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        logoView.layer.cornerRadius = 15
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        animationSettingDefault()
+        appearLogoViewAnimation { [weak self] in
+            self?.slideBackgroundImageAnimation()
+            self?.blinkLogoAnimation()
+        }
+    }
+    
+    private func animationSettingDefault() {
+        logoViewTopConstraint.constant = -200
+        backgroundImageLeadingConstraint.constant = 0
+        logoView.alpha = 1
+        view.layoutIfNeeded()
+    }
+    
+    private func appearLogoViewAnimation(completion: @escaping () -> Void) {
+        UIView.animate(withDuration: 1,
+                       delay: 1,
+                       usingSpringWithDamping: 0.6,
+                       initialSpringVelocity: 1,
+                       options: []) { [weak self] in
+            self?.logoViewTopConstraint.constant = 17
+            self?.view.layoutIfNeeded()
+        } completion: { _ in
+            completion()
+        }
+    }
+    
+    private func slideBackgroundImageAnimation() {
+        UIView.animate(withDuration: 5, delay: 0, options: [.curveEaseInOut, .repeat, .autoreverse]) { [weak self] in
+            self?.backgroundImageLeadingConstraint.constant = -800
+            self?.view.layoutIfNeeded()
+        }
+    }
+    
+    private func blinkLogoAnimation() {
+        UIView.animate(withDuration: 1, delay: 0, options: [.repeat, .autoreverse]) { [weak self] in
+            self?.logoView.alpha = 0
+        }
+    }
 }
 
